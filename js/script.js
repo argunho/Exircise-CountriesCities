de9c6ec05d7edc3a3e086c80c0f489368f82cf95
argunho <aslan_argun@hotmail.com>
2021-11-11 15:03:37 +0100

    Exircise-CountriesCities

---
+++ js/script.js
@@ -0,0 +1,221 @@
+"use strict";
+
+var loc = localStorage;
+const countries_link = "https://raw.githubusercontent.com/argunho/assignments/master/Exircise-CountriesCities/json/land.json";
+const cities_link = "https://raw.githubusercontent.com/argunho/assignments/master/Exircise-CountriesCities/json/stad.json";
+
+var countries, cities = [];
+var visited_list = [];
+var reset_btn = document.getElementById("reset")
+var view = document.getElementById("view");
+var content = document.getElementById("content");
+
+document.body.style = "background:url('./images/mapp.jpg');" +
+    "background-position:center;" +
+    "background-size:cover;" +
+    "background-repeat:no-repeat;";
+
+fetch(countries_link).then(function(res) {
+    return res.json();
+}).then(function(list) {
+    countries = JSON.parse(JSON.stringify(list));
+
+    // Run set countries function
+    setCountriesMenu();
+})
+
+// Set countries list with restart of page
+function setCountriesMenu() {
+    if (loc.getItem("current") != null)
+        loc.removeItem("current");
+    if (JSON.parse(loc.getItem("visited_list")) != null) {
+        reset_btn.classList.add("visible-reset");
+    };
+
+    checkMenuList()
+}
+
+// Check menu list
+function checkMenuList() {
+    let visited_cities = JSON.parse(loc.getItem("visited_list"));
+    let menu = document.getElementById("menu");
+    menu.innerHTML = "";
+    if (countries.length > 3)
+        countries.splice(3, 1);
+
+    if (visited_cities != null) {
+        let obj = {
+            "id": 4,
+            "name": "Städer jag besökt"
+        }
+        countries.push(obj)
+    }
+
+    for (let i in countries) {
+        let c = countries[i];
+        let name = (c.id < 4) ? c.countryname : c.name;
+        let _name = (c.id < 4) ? c.countryname : "visited";
+        menu.insertAdjacentHTML("beforeend", "<div class='menu-link'><p class='p-link' name='" + _name.toLowerCase() + "' onclick='getCities(this)' id='list_" + c.id + "'>" +
+            name + "</p><ul class='menu-list' id='menu_list_" + c.id + "'></ul></div>");
+    }
+}
+
+// Get sub menu(cities list)
+function getCities(ev) {
+    let id = ev.attributes.id.value;
+    let name = ev.attributes.name.value;
+
+    reset(true);
+    if (loc.getItem("current") == name) {
+        loc.removeItem("current");
+    } else {
+        loc.setItem("current", name);
+
+        loc.setItem(name, id);
+
+        id = parseInt(id.replace("list_", ""));
+        ev.classList.add("selected-" + name);
+
+        let ul = document.getElementById("menu_list_" + id);
+        ul.classList.add('visible-menu-list', 'ul_' + name);
+
+        fetch(cities_link).then(function(res) {
+            return res.json();
+        }).then(function(list) {
+            cities = JSON.parse(JSON.stringify(list));
+
+            let cities_list = (id < 4) ? cities.filter(x => x.countryid == id) : JSON.parse(loc.getItem("visited_list"));
+            for (let i in cities_list) {
+                let c = cities_list[i];
+                ul.insertAdjacentHTML("beforeend", "<li class='link' onclick='getCityInfo(this)' name='" + c.stadname.toLowerCase() + "' id='link_" + c.id + "'>" + c.stadname + "</li>");
+            }
+            if (id == 4) {
+                ul.insertAdjacentHTML("beforeend", "<li class='link' onclick='getPopulations()' name='population' id='link_population'>Antal invånare i besökta länder</li>");
+            }
+        })
+
+    }
+}
+
+// Get city info
+function getCityInfo(ev) {
+    let id = ev.attributes.id.value;
+    let name = ev.attributes.name.value;
+    reset();
+    if (loc.getItem(name) == null)
+        loc.setItem(name, id);
+
+    content.classList.remove("none");
+    let city = cities.find(x => x.id == parseInt(id.replace("link_", '')));
+
+    view.style.background = "url('" + city.img + "') #FFF";
+    content.insertAdjacentHTML("beforeend", "<p class='city-name'>" + city.stadname + "</p>");
+    content.insertAdjacentHTML("beforeend", "<p class='city-population'>Invånare: " + city.population + "</p>");
+    content.insertAdjacentHTML("beforeend", "<button class='make-visited' type='button' title='Besökt' onclick='setVisitedList(" + city.id + ")' id='visited_btn'>🏴</button>");
+
+    setVisitedList(city.id, true);
+    setTimeout(function() {
+        setOpacity();
+    }, 300)
+}
+
+// Set and save visited list
+function setVisitedList(id, check = false) {
+    visited_list = JSON.parse(loc.getItem("visited_list")) || [];
+    let visited = false;
+    if (visited_list.length > 0)
+        visited = visited_list.find(x => x.id == id) != null;
+
+    let city = cities.find(x => x.id == id);
+    let btn = document.getElementById("visited_btn");
+
+    if (!check) {
+        if (!visited) {
+            btn.classList.add("visited-btn");
+            btn.innerHTML = '🏁';
+            visited_list.push(city);
+        } else {
+            btn.classList.remove("visited-btn");
+            btn.innerHTML = '🏴';
+            for (let i in visited_list) {
+                if (city.id == visited_list[i].id)
+                    visited_list.splice(i, 1);
+            }
+        }
+    } else if (visited) {
+        btn.classList.add("visited-btn");
+        btn.innerHTML = '🏁';
+    }
+    if (visited_list.length > 0) {
+        loc.setItem("visited_list", JSON.stringify(visited_list));
+        reset_btn.classList.add("visible-reset");
+    } else {
+        reset_btn.classList.remove("visible-reset");
+        loc.removeItem("visited_list");
+    }
+
+    checkMenuList();
+}
+
+// Get population of visted cities
+function getPopulations() {
+    reset();
+    let population = 0;
+    let list = JSON.parse(loc.getItem("visited_list"))
+    for (let i in list) {
+        population += list[i].population;
+    }
+    view.style.background = "#FFF";
+    setOpacity();
+    content.classList.add("none");
+    view.insertAdjacentHTML("beforeend", "<div class='populations-count' id='population'>" + population + "</div>")
+}
+
+// Reset menu
+function reset(reset = false) {
+    document.querySelectorAll("ul").forEach(function(a, b) {
+        a.classList.remove('visible-menu-list');
+        a.innerHTML = "";
+    });
+    document.querySelectorAll("#content>p").forEach(function(a, b) {
+        a.innerHTML = "";
+    })
+    document.querySelectorAll("#content>button").forEach(function(a, b) {
+        a.remove();
+    })
+    if (reset) {
+        document.querySelectorAll("p.p-link").forEach(function(a, b) {
+            a.classList.remove("selected-" + a.attributes.name.value);
+        })
+    }
+
+    let view = document.getElementById("view");
+    view.removeAttribute("style");
+    view.classList.remove("visible-view");
+
+    let population = document.getElementById('population');
+    if (population != undefined)
+        population.parentNode.removeChild(population);
+}
+
+// Clear history
+function clearHistory() {
+    reset();
+    loc.clear();
+    checkMenuList();
+    reset_btn.classList.remove("visible-reset");
+}
+
+// Help function
+function setOpacity() {
+    let scale = 0.1;
+    let opacity = setInterval(function() {
+        if (scale > 1)
+            clearInterval(opacity);
+        else {
+            view.classList.add("visible-view")
+            view.style.opacity = scale;
+        }
+        scale += 0.1;
+    }, 100);
+}
---
+++ readme.txt
@@ -0,0 +1,43 @@
+Inlämningsuppgift: Städer och länder
+
+01. Function setCountriesMenu()     = Körs sidan start eller omstart                        
+02. Function checkMenuList()        = Körs i flera funktionen och kontrollerar meny länks, 
+                                            lägger till och tar bort extra länk för besökta städer
+03. Function getCities()            = Körs vid klick på land i menyn att öppna undermeny med städer respektive land
+04. Function getCityInfo()          = Körs vid klick på stad i undermenyn att öppna vy med stads informationen
+05: Function setVistedList()        = Körs vid klick på knapp att märka staden som besökt stad och att spara information 
+                                            i localStorage och lägga eller ta bort stad från besökta listan
+06. Function getPopulations()       = Körs vid klick på länken i undermeny för Besökta länder att räkna ut total antal
+                                            stadsinvånare i alla besökta städer
+07. Function reset()                = Körs att återställa alla ändringar i koden
+08. Function clearHistroy()         = Körs vid klick på knappen Rensa historik att rensa all sparad information i localStorage
+                                            och samt återställa ändringar
+09. Function setOpacity()           = Körs i två funktionen att slippa skriva samma kod två gånger och avsett för att öka 
+                                            vy opacity från 0 till 1 småningom
+
+
+Mina tanke och syfte med beskrivan funktionerna ovan
+
+01. Function setCountriesMenu()     = Förberedda sidan för användare med rätt strukturerad meny
+                                      Kontrollera behövs eller inte visa Återställning knap vid omstart
+                                      Raderar sparad nyckelord "current" i localStorage varje gång när sidan startas, vad är det
+                                             för "current" - detta sparas först i funktionen getCities() för att kontrollera
+                                             var meny med land namnet redan klickat eller inte, vi upprepande klick stänger öppnad undermeny
+02. Function checkMenuList()        = Tanke med denna, att kolla i localStorage finns besökta städer eller inte, varje gång när fiktionen körs 
+                                             raderas meny och ställas på nytt. Om det finns besökta länder som sparad de läggs till en extra
+                                             objekt i lands listan och kommer med detta visas en extra block i menyn att visa besökta länder. Vid fall 
+                                             om användare rensar historik eller raderar land från besökta sidan körs funktionen igen. Samt avsett funktionen    
+                                             för design också, lägger ill. meny länkar speciella class för respektive land
+03. Function getCities()            = Denna menyn avsett att öppna undermenyn med rätt listan av städer som tillhör till den land som var klickad.
+                                      Här sparas nyckelord "current" som jag pratade om innan och lägger till vissa class namn till elemnter att göra
+                                             den fint och funktionella vid klick på Besökta länder lägger till städer lista extra länk at kolla på invånarens antal.
+04. Function getCityIfo()        = Söker denna stad som motsvarar id som var mottagen, lägger till elementen som var tagen getElementById
+                                             och i funktionen körs tre andra funktioner som har sitt eget syfte.
+05. Function setVisitedList()       = Tar emot stad id att lägga till localStorage besökta listan eller tar bort den genom att rensa hela besökta listan från localStorage
+                                             och lägger till den på nytt. När markeras eller avmarkeras stade bytes lite design på knappen. Samt funktionen 
+                                             när denna körs i getCitiesInfo() avsett att kontrollera vid sidans start eller omstart var detta stad sparad 
+                                             innan eller inte om den är hittad som sparad, då blir knappen ”Besökta” med design som redan klickad
+
+
+                                                                                                                           
+Resten av funktionerna redan beskrivna ovan
